ed067f646981382ca70a2a900e18c8a224969cc9
Sokdara Cheng <chengsokdara@gmail.com>
2025-05-23 14:11:06 +0700

    initial setup complete

---
+++ src/app/api/magic/route.ts
@@ -0,0 +1,32 @@
+import { stringify } from "@/utils";
+import { NextRequest, NextResponse } from "next/server";
+
+const API_PATH = "/api/postmark/inbound/webhook";
+
+export async function GET(request: NextRequest) {
+  try {
+    const data = await handler(request);
+    return NextResponse.json(
+      { data, message: "Hello World!" },
+      { status: 200, statusText: "OK" },
+    );
+  } catch (error) {
+    console.warn(API_PATH, { error: stringify(error) });
+    return NextResponse.json(
+      { error },
+      { status: 500, statusText: "Internal Server Errror" },
+    );
+  }
+}
+
+async function handler(request: NextRequest) {
+  const LOG_NAME = API_PATH + "." + handler.name;
+  try {
+    const body = Object.fromEntries(request.nextUrl.searchParams.entries());
+    console.info(API_PATH, { body: stringify(body) });
+    return body;
+  } catch (error) {
+    console.warn(LOG_NAME, { error: stringify(error) });
+    throw error;
+  }
+}
---
+++ src/app/content.tsx
@@ -0,0 +1,9 @@
+export function HomeContent() {
+  return (
+    <main className="flex flex-col flex-1 h-full bg-base-200 xl:mx-auto xl:max-w-7xl">
+      <section className="flex flex-col flex-1 items-center justify-center">
+        <button className="btn btn-primary">Hello World</button>
+      </section>
+    </main>
+  );
+}
